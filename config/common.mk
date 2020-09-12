15ccda1f53f3e5803f37f39379f1284d089213d7
Alexander Koskovich <zvnexus@outlook.com>
2020-09-12 09:09:08 -0700

    vendor: Include overlays for some AOSP customizations
     * Structure so far:
    
     * ├── aosp
       ├── google
       │   ├── gms
       │   ├── modules
       │   ├── pixel
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -229,6 +229,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CustomConfigOverlay \
+    CustomLauncherOverlay \
+    CustomSettingsOverlay
+    
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
 
