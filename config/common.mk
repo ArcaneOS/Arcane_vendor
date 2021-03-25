ca3c2968c09f998c837d5ac3478b9eed74f6146a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-25 16:02:22 +0530

    overlay/props: Properly enable iorap tracing and readahead
     * iorapd reads these configs through device_config so we must
       enable it the same way

--- config/common.mk
+++ config/common.mk
@@ -177,6 +177,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # IORap app launch prefetching using Perfetto traces and madvise
 PRODUCT_PRODUCT_PROPERTIES += \
+    iorapd.perfetto.enable=true \
+    iorapd.readahead.enable=true \
+    persist.device_config.runtime_native_boot.iorap_perfetto_enable=true \
+    persist.device_config.runtime_native_boot.iorap_readahead_enable=true \
     ro.iorapd.enable=true
 
 # Face Unlock
