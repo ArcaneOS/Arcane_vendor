d0cf84acbf9e0c3456d7e4e3e6406471ae63cdf1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-17 18:17:15 +0530

    overlay: Add Primer Blue accent color
     * based on https://github.com/ProtonAOSP/android_vendor_proton/commit/f5f64a86e3c00cfe6cfc60a70a2f9db759bb20bd
    
    Change-Id: I693ec4d7a4d3483c62dba3709d1468db0c37a244
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -16,6 +16,7 @@ PRODUCT_PACKAGES += \
     FontGoogleSansRobotoOverlay \
     AccentColorArcaneOverlay \
     AccentColorParanoidOverlay \
+    AccentColorPrimerBlueOverlay \
     FontSamOverlay \
     FontVictorOverlay
 
