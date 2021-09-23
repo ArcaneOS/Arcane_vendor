5f6e871a686d276199c128a4010c3aff43332a32
dlwlrma123 <alexfinhart@gmail.com>
2021-09-23 13:14:25 +0800

    overlay: Add and set Paranoid accent as default
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
     FontKaiOverlay \
     FontRubikRubikOverlay \
     FontGoogleSansRobotoOverlay \
+    AccentColorParanoidOverlay \
     FontSamOverlay \
     FontVictorOverlay
 
